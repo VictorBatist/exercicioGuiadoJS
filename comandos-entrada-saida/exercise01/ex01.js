ce99f19ea48e01bfa982fae7b5deb2abc631c9f2
Victor Batista <vbatista.dev@gmail.com>
2024-05-13 20:27:41 -0300

    Exercise 01

---
+++ comandos-entrada-saida/exercise01/ex01.js
@@ -0,0 +1,7 @@
+let numero1 = Number(window.prompt('Informe um numero:'))
+let numero2 = Number(window.prompt('Informe outro numero:'))
+let numero3 = Number(window.prompt('Informe outro numero:'))
+
+let soma = numero1 + numero2 + numero3
+
+alert(`A soma dos valores ${numero1} + ${numero2} + ${numero3} é ` + soma) 
